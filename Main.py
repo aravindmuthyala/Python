a7b3404a42b66eb913db9d0ea21b1aeac5180d6f
aravindmuthyala <aravind.muthyala@unvired.io>
2023-07-13 17:14:50 +0530

    initial commit

---
+++ Main.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+import openai
+import langchain
+from langchain.llms import OpenAI
+
+
+load_dotenv()
+my_key = os.getenv("OPENAI_API_KEY")
+
+llm = OpenAI(temperature=0.0,openai_api_key=my_key)
+text= "what is AI?"
+print(llm(text))
